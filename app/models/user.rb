1c4eeaf25149b631be13648acfa27442922f7a5f
hediessid <essidhedi7000@gmail.com>
2015-12-31 13:42:09 -0500

    articles show and index

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ActiveRecord:base
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151225191804) do
+ActiveRecord::Schema.define(version: 20151231180232) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text   "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
